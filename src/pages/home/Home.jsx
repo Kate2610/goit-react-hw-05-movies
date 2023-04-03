74f659bf8e096e70ab5c7a512439063faf6dead9
Kate2610 <yekaterina.anisimova@gmail.com>
2023-04-03 17:37:55 +0300

    new

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import { getTranding } from 'Services/api';
+import { toast } from 'react-toastify';
+import { MagnifyingGlass } from 'react-loader-spinner';
+import { MoviesList } from 'components/moviesList/MoviesList';
+
+export const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      setError('');
+      try {
+        const data = await getTranding();
+        setMovies(data.results);
+      } catch (error) {
+        setError('There is something wrong in your action');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchMovie();
+  }, []);
+  useEffect(() => {
+    if (!error) return;
+    toast.error(error);
+  }, [error]);
+
+  return (
+    <section>
+      <h1>Tranding today</h1>
+      {movies.length > 0 && <MoviesList movies={movies} />}
+      {isLoading && (
+        <MagnifyingGlass
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+        />
+      )}
+    </section>
+  );
+};
